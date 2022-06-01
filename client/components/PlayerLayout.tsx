9b1072b20fcc1a969b45952b432fa334dab87a54
hs-fkisuule <farouk.kisuule@homestars.com>
2022-06-01 15:18:56 -0400

    Added theme and wrapped it around different componenets and also tested out player layout component

---
+++ client/components/PlayerLayout.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/layout";
+import React from "react";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      Layout
+      {children}
+    </Box>
+  );
+};
+
+export default PlayerLayout;
