7ad3e3bdf9bb046fcc04f3252343e1801ef56b77
hs-fkisuule <farouk.kisuule@homestars.com>
2022-08-09 21:00:43 -0400

    Added form submission

--- client/components/authForm.tsx
+++ client/components/authForm.tsx
@@ -1,27 +1,72 @@
 import { Box, Flex, Input, Button } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { FC, useState } from "react";
 import { useSWRConfig } from "swr"; // it handles data fetching/caching/refetching/re-evaluate/update local cache
 import { auth } from "../lib/mutations";
+import NextImage from "next/image";
 
-const AuthForm: FC<{ mode: string }> = ({ mode }) => {
+const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    await auth(mode, { email, password });
+    setIsLoading(false);
+    router.push("/");
+  };
 
   return (
     <Box height="100vh" width="100vw" bg="black">
-      <Flex justify="center" align="center" height="100px" color="white">
-        Hello There
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        color="white"
+        borderBottom="white 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
       <Flex
         justify="center"
         align="center"
-        height="calc(100vh - 100vw"
+        height="calc(100vh - 100px)"
         color="white"
       >
-        Form
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Button
+              type="submit"
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{
+                "&:hover": {
+                  bg: "green.300",
+                },
+              }}
+            >
+              {mode}
+            </Button>
+          </form>
+        </Box>
       </Flex>
     </Box>
   );
